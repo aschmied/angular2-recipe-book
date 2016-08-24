0e231cf8b7a4607498bdf4271df9fdb34a085d67
Anthony Schmieder <anthony@primatelabs.com>
2016-08-23 22:15:55 -0400

    Add stub RecipesComponent

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header.component';
+import { RecipesComponent } from './recipes';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    RecipesComponent
   ],
   imports: [BrowserModule, FormsModule],
   bootstrap: [AppComponent],
