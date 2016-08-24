c1950af5320f19ce2b07396a5f6157de0102bbf5
Anthony Schmieder <anthony@primatelabs.com>
2016-08-23 22:04:09 -0400

    Add HeaderComponent to AppComponent

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent
   ],
   imports: [BrowserModule, FormsModule],
   bootstrap: [AppComponent],
